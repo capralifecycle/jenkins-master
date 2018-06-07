6e568c846d14cb7d2238f3304fe0987b6b85e1a3
Henrik Steen <henrist@henrist.net>
2018-06-07 19:18:00 +0200

    Update script to fetch plugins (need GitHub user)

--- plugin-history/README.md
+++ plugin-history/README.md
@@ -7,6 +7,12 @@ be diffed against an earlier list. Also kept as a reference to initial plugins.
 
 ## Updating the list
 
+You need to log in to Jenkins by using a GitHub Personal Access Token that
+only needs the read:org scope. For reference see
+https://wiki.jenkins.io/display/JENKINS/GitHub+OAuth+Plugin
+
+The script will ask for your username and token.
+
 ```bash
 ./update.sh
 ```
--- plugin-history/update.sh
+++ plugin-history/update.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+set -e
 
-curl -sSL -u capraadmin "https://jenkins.capra.tv/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins" \
+echo -n "GitHub username: "
+read username
+
+echo "You will now be asked for a password. Enter your GitHub Personal Access Token created for this use."
+
+curl -sSL -u "$username" "https://jenkins.capra.tv/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins" \
   | perl -pe 's/.*?<shortName>([\w-]+).*?<version>([^<]+)()(<\/\w+>)+/\1 \2\n/g' \
   | sed 's/ /:/' \
   | sort >plugin-list.txt
