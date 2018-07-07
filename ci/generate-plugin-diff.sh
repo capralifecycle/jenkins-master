a82d3c406567d1a3c2b8f5d530527e3b987d0453
Henrik Steen <henrist@henrist.net>
2018-07-07 15:15:50 +0200

    Fix/improve plugin diff header

--- ci/generate-plugin-diff.sh
+++ ci/generate-plugin-diff.sh
@@ -3,7 +3,8 @@ set -e
 
 diff_file=plugin-history/plugin-list-prod.diff
 
-echo "build-from-source                                                  jenkins.capra.tv" >$diff_file
+echo "jenkins.capra.tv                                                   build-from-source" >$diff_file
+echo "----------------                                                   -----------------" >>$diff_file
 (diff -ty plugin-history/plugin-list-prod.txt plugin-history/plugin-list-build.txt || :) >>$diff_file
 
 echo "Plugin diff:"
